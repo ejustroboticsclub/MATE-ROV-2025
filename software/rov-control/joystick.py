5660472557dc7711f61ad9235de29b6ef0ca2634
Abdelrhman Eldenary <abdelrhmaneldenary@gmail.com>
2025-03-07 22:36:04 +0200

    update files for rotation task of the ROV

--- software/rov-control/joystick.py
+++ software/rov-control/joystick.py
@@ -12,12 +12,19 @@ class JoyStickNode(Node):
         self.joystick_publisher = self.create_publisher(Twist, "ROV/joystick", 10)
         self.gripper_r_publisher = self.create_publisher(Bool, "ROV/gripper_r", 1)
         self.gripper_l_publisher = self.create_publisher(Bool, "ROV/gripper_l", 1)
+        ###
+        self.rotatingROV_publisher=self.create_publisher(Bool, "ROV/rotating", 1)
+        ###
         self.timer = self.create_timer(0.01, self.update)
         self.controller = get_controller()
 
         self.twist_msg = Twist()
         self.gripper_r_msg = Bool()
         self.gripper_l_msg = Bool()
+        
+        ###
+        self.rotatingROV_msg=Bool()
+        ###
 
     def update(self):
         self.controller.update()
@@ -31,6 +38,10 @@ class JoyStickNode(Node):
 
         self.controller.leftGripper()
         self.controller.rightGripper()
+
+        ###
+        self.controller.rotatingROV()
+        ###
 
         self.twist_msg.linear.x = (
             float(self.controller.getPitch()) if not aim_ball_x else 0.0
@@ -48,6 +59,10 @@ class JoyStickNode(Node):
         self.gripper_r_msg.data = self.controller.right_gripper
         self.gripper_l_msg.data = self.controller.left_gripper
 
+        ###
+        self.rotatingROV_msg.data=self.controller.rotating_button
+        ###
+
         # self.get_logger().info("Throttle: %+2.2f   Roll: %+2.2f   Pitch: %+2.2f   Yaw: %+2.2f   Aux: %+2.2f"
         #     % (
         #         self.controller.depth,
@@ -59,6 +74,11 @@ class JoyStickNode(Node):
 
         self.gripper_r_publisher.publish(self.gripper_r_msg)
         self.gripper_l_publisher.publish(self.gripper_l_msg)
+
+        ###
+        self.rotatingROV_publisher.publish(self.rotatingROV_msg)
+        ###
+
 
         if self.controller.stopAll():
             self.twist_msg = Twist()
