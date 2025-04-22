ffd877fb490137f935d55c426b22454d3d74dceb
rm-rf-humans <nigganigga@duck.com>
2025-04-23 01:10:35 +0200

    add: sending currents and voltages over ROS Topics; adding to that, 3 heartbeats msg (Int8MultiArray)

--- software/Embedded/STM_DEBUGGER/STM_DEBUGGER.ino
+++ software/Embedded/STM_DEBUGGER/STM_DEBUGGER.ino
@@ -1,12 +1,28 @@
+#define TARGET_STM32F4  // For Micro-Ros Library to Operate Correctly
+
 #include <Wire.h>
 #include <SPI.h>
 #include <mcp_can.h>
 #include "MS5837.h"
+#include <LoayEthernetUdp.h>
+#include <LoayEthernet.h>
+#include <SPI.h>
+#include <micro_ros_arduino.h>
+#include <native_ethernet_transport.cpp>
 
-// CAN constants
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/float32.h>  // For publishing Float32 (voltage and current)
+#include <std_msgs/msg/int8_multi_array.h>  // For publishing Int8MultiArray (heartbeat)
+
+#define W5500_CS_pin PA15
 #define SPI_CS_PIN PB8
-#define DEPTH_ID   0x205
-#define Pump_ID    0x300
+#define SCLK_pin PA5
+#define MISO_pin PA6
+#define MOSI_pin PA7
 #define Currents_ID1 0x310
 #define Currents_ID2 0x312
 #define Currents_ID3 0x315
@@ -14,9 +30,6 @@
 #define Voltages_ID1 0x110
 #define Voltages_ID2 0x112
 #define Voltages_ID3 0x115
-#define Thrusters_ID 0x100
-#define Grippers_ID_R  0x101
-#define Grippers_ID_L  0x102
 
 // Heartbeat CAN IDs
 #define HEARTBEAT_ID1 0x320
@@ -25,8 +38,30 @@
 
 MCP_CAN CAN(SPI_CS_PIN);
 
+IPAddress Agent_IP(192, 168, 1, 101); 
+IPAddress STM_IP(192, 168, 1, 105); 
+
+
+// ROS Publishers
+rcl_publisher_t publisher_voltage;
+rcl_publisher_t publisher_current;
+rcl_publisher_t publisher_heartbeat;
+
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rclc_executor_t executor;
+
+
+int8_t heartbeat_values[3];  // Store values for HEARTBEAT_ID1, HEARTBEAT_ID2, and HEARTBEAT_ID3
+int heartbeat_index = 0;  // To track which heartbeat we're receiving
+
 void setup() {
   Serial.begin(115200);
+
+  byte STM_MACaddress[] = { 0xAA, 0xEE, 0xCC, 0xEE, 0xDD, 0xEE };
+  set_microros_native_ethernet_udp_transports(STM_MACaddress, STM_IP, Agent_IP, 9999, W5500_CS_pin, SCLK_pin, MISO_pin, MOSI_pin);
+
   Serial.println("CAN Receiver Ready...");
 
   if (CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
@@ -38,6 +73,17 @@ void setup() {
 
   CAN.setMode(MCP_NORMAL);
   Serial.println("Waiting for all CAN data...");
+
+  rclc_support_init(&support, 0, NULL, &allocator);
+  rclc_node_init_default(&node, "rov_node", "", &support);
+  
+  rclc_publisher_init_default(&publisher_voltage, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "ROV/voltage");
+  rclc_publisher_init_default(&publisher_current, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "ROV/current");
+  rclc_publisher_init_default(&publisher_heartbeat, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int8MultiArray), "ROV/indicators");
+
+  // Initialize executor
+  rclc_executor_init(&executor, &support.context, 1, &allocator);
+  Serial.println("ROS Initialized.");
 }
 
 void loop() {
@@ -58,6 +104,8 @@ void loop() {
       handleHeartbeat(rxId, data[0]);
     }
   }
+
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
 }
 
 void printCurrentData(unsigned long rxId, byte* data, byte len) {
@@ -71,6 +119,12 @@ void printCurrentData(unsigned long rxId, byte* data, byte len) {
       float current = raw / 100.0;
       Serial.print(current, 2);
       Serial.print(" A");
+
+      // Publish current to ROS topic
+      std_msgs__msg__Float32 current_msg;
+      current_msg.data = current;
+      rcl_publish(&publisher_current, &current_msg, NULL);
+
       if (i + 2 < len) Serial.print(" | ");
     }
   }
@@ -88,6 +142,12 @@ void printVoltageData(unsigned long rxId, byte* data, byte len) {
       float voltage = raw / 100.0;
       Serial.print(voltage, 2);
       Serial.print(" V");
+
+      // Publish voltage to ROS topic
+      std_msgs__msg__Float32 voltage_msg;
+      voltage_msg.data = voltage;
+      rcl_publish(&publisher_voltage, &voltage_msg, NULL);
+
       if (i + 2 < len) Serial.print(" | ");
     }
   }
@@ -98,6 +158,15 @@ void handleHeartbeat(unsigned long rxId, uint8_t value) {
   Serial.print("Heartbeat from 0x");
   Serial.print(rxId, HEX);
   Serial.print(": ");
+  
+  if (rxId == HEARTBEAT_ID1) {
+    heartbeat_values[0] = value;
+  } else if (rxId == HEARTBEAT_ID2) {
+    heartbeat_values[1] = value;
+  } else if (rxId == HEARTBEAT_ID3) {
+    heartbeat_values[2] = value;
+  }
+
   if (value == 1) {
     Serial.println("Alive");
   } else if (value == 0) {
@@ -105,5 +174,18 @@ void handleHeartbeat(unsigned long rxId, uint8_t value) {
   } else {
     Serial.print("Unknown value: ");
     Serial.println(value);
+  }
+
+  if (heartbeat_index == 2) {
+    std_msgs__msg__Int8MultiArray heartbeat_msg;
+    heartbeat_msg.data.data = heartbeat_values;  
+    heartbeat_msg.data.size = 3; 
+    heartbeat_msg.data.capacity = 3;  
+
+    rcl_publish(&publisher_heartbeat, &heartbeat_msg, NULL);
+
+    heartbeat_index = 0;
+  } else {
+    heartbeat_index++;
   }
 }
